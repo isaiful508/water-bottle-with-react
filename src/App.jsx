f32033e1f63aadb86cc6e92bdc9de5b1a307e781
isaiful508 <isaiful.islam508@gmail.com>
2024-03-12 23:41:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Bottles from './componenets/header/Bottles/Bottles'
+import Header from './componenets/header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Bottles></Bottles>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/componenets/header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Memorable water bottle</h2>
+        </div>
+    );
+};
+
+export default Header;
