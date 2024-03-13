3608bd0c168049832557f893e1cb2c93db841b74
isaiful508 <isaiful.islam508@gmail.com>
2024-03-13 16:13:29 +0600

    added local storag js fiel

---
+++ src/utilities/localStorage.js
@@ -0,0 +1,24 @@
+const getStoredCart = () =>{
+    const storedCartString = localStorage.getItem('cart')
+
+    if(storedCartString){
+        return JSON.parse(storedCartString);
+    }
+    return [];
+}
+
+const savedCartToLS = cart => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+} 
+
+
+const addToLS = (id) => {
+    const cart = getStoredCart();
+    cart.push(id);
+
+    //saved to local storage
+    savedCartToLS(cart)
+}
+
+export { addToLS }
