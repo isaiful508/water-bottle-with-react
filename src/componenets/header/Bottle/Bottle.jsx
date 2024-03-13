ee6756143b2688cf9fa5390aaa7d54eab1d36a59
isaiful508 <isaiful.islam508@gmail.com>
2024-03-13 15:17:31 +0600

    added react project

--- src/componenets/header/Bottle/Bottle.jsx
+++ src/componenets/header/Bottle/Bottle.jsx
@@ -1,12 +1,13 @@
 import './Bottle.css'
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleAddCart}) => {
    const {name, img, price} = bottle
     return (
         <div className="bottle">
             <h3>Bottle: {name}</h3>
             <img src={img} alt="" />
             <p>Price:$ {price}</p>
+            <button onClick={() => handleAddCart(bottle)}>Purchase</button>
         </div>
     );
 };
--- src/componenets/header/Bottles/Bottles.jsx
+++ src/componenets/header/Bottles/Bottles.jsx
@@ -6,22 +6,38 @@ import './Bootles.css'
 
 const Bottles = () => {
 
-    const [bottles, setBottles] = useState([])
+    const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
+
+
+
+
     useEffect( () =>{
         fetch('bottles.json')
         .then(res => res.json())
         .then(data => setBottles(data));
     }, [])
 
+
+const handleAddCart = (bottle) =>{
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+}
+
+
+
+
     return (
         <div>
-            <h2>Bottles here: {bottles.length}</h2>
+            <h2>Bottles Available: {bottles.length}</h2>
+            <h4>Cart: {cart.length}</h4>
           <div className="bottle-conatiner">
             
           {
                 bottles.map( bottle => <Bottle
                 key={bottle.id}
                 bottle={bottle}
+                handleAddCart={handleAddCart}
                 ></Bottle>)
             }
             
